ca25ad81bc5e28b36c5fe0f4f6d8c72923bd2b7b
dalecgu <131250169@smail.nju.edu.cn>
2015-05-31 21:47:05 +0800

    增加了爬球员头像的功能

--- src/logic/playerlogic/PlayerLogic.java
+++ src/logic/playerlogic/PlayerLogic.java
@@ -1,5 +1,11 @@
 package logic.playerlogic;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
@@ -17,6 +23,7 @@ import logicservice.playerlogicservice.PlayerLogicService;
 public class PlayerLogic implements PlayerLogicService {
 
 	final String INDEX_URL = "http://china.nba.com";
+	final String DATA_PATH = "./res";
 	
 	public void getPlayerBasicInfo() {
 		WebClient webClient = new WebClient();
@@ -52,12 +59,43 @@ public class PlayerLogic implements PlayerLogicService {
 			e.printStackTrace();
 		}
 		
-		//TODO 处理list
-		//for (PlayerBasicInfo playerBasicInfo : list) {
-		//	System.out.println(playerBasicInfo);
-		//}
-		
 		webClient.close();
+		
+		//TODO 处理list
+		for (PlayerBasicInfo playerBasicInfo : list) {
+			getPlayerHeadPicture(playerBasicInfo, DATA_PATH + "/player/head/");
+		}
+	}
+	
+	private void getPlayerHeadPicture(PlayerBasicInfo player, String path) {
+		try {
+			String playerPath = player.getPath();
+			int i = playerPath.lastIndexOf("/");
+			String id = playerPath.substring(i+1, playerPath.lastIndexOf("_"));
+			String url = "http://china.nba.com/media/img/players/head/230x185/"
+					+ id + ".png";
+			BufferedInputStream in;
+			try {
+				in = new BufferedInputStream(new URL(url).openStream());
+			} catch (FileNotFoundException e) {
+				in = new BufferedInputStream(
+						new URL("http://china.nba.com/media/img/players/head/230x185/"
+								+ "not_found.png").openStream());
+			}
+			
+			BufferedOutputStream out = new BufferedOutputStream(
+					new FileOutputStream(new File(path + player.getEnglishName() + ".png")));
+			byte[] buff = new byte[2048];
+			int length = in.read(buff);
+			while (length != -1) {
+				out.write(buff, 0, length);
+				length = in.read(buff);
+			}
+			in.close();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
