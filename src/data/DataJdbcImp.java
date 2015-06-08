d93b0eb9466cd2a8521e0fb53b58de024af9016a
crosseveryriver <1304138563@qq.com>
2015-06-08 16:33:59 +0800

    Database

--- src/data/DataJdbcImp.java
+++ src/data/DataJdbcImp.java
@@ -18,25 +18,57 @@ public class DataJdbcImp {
 		
 	}
 	public void checkSeason(String season){
-		String MatchSql = "drop table if exists Match"+season+"Season"+";create table Match"+season+"Season"+" "+"(MatchID varchar(25) primary key,HomeCourtTeamAbb varchar(5),"
+		Statement stat;
+		boolean tableExists = true;
+		try {
+			stat = connection.createStatement();
+			stat.execute("select * from Match"+season+"Season");
+		} catch (SQLException e1) {
+			tableExists = false;
+		}
+		
+		String MatchSql = "create table Match"+season+"Season"+" "+"(MatchID varchar(25) primary key,HomeCourtTeamAbb varchar(5),"
 				+ "AwayTeamAbb varchar(5),DateOfMatch varchar(10),HomeScore1 Integer,"
 				+ "HomeScore2 Integer,HomeScore3 Integer,HomeScore4 Integer,AwayScore1 Integer,"
 				+ "AwayScore2 Integer,AwayScore3 Integer,AwayScore4 Integer,HomeScore Integer,"
-				+ "AwayScore Integer,Overtime Integer) ";
-		Statement stat;
-		try {
-			stat = connection.createStatement();
-			stat.execute(MatchSql);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				+ "AwayScore Integer,Overtime Integer);";
+		String OvertimeSql ="create table OverTime"+season+"Season(MatchID varchar(25) primary key,SerialNumber Integer,HomeScore Integer,AwayScore Integer);";
+		String PlayerSql="create table Player"+season+"Season(PlayerName varchar(10) primary key,TeamAbb varchar(4),NumOfMatch Integer,NumOfStart Integer,Rebounds Integer,"
+				+ "Assists Integer,PresenceTime Integer,ShootingPersentage Double,ThreePointPersentage Double,FreeThrowPersentage Double,"
+				+ "OffensiveRebounds Integer,DefensiveRebounds Integer,Steals Integer,BlockShots Integer,TurnOvers Integer,Fouls Integer,Score Integer,Efficiency Double,"
+				+ "GmSc Double,RealShootingPersentage Double,ShootingEfficiency Double,ReboundRate Double,OffensiveReboundRate Double,DefensiveReboundRate Double,"
+				+ "AssistRate Double,StealRate Double,BlockShotRate Double,TurnOverRate Double,UseRate Double );";
+		String PlayerMatchSql="create table PlayerMatch"+season+"Season(MatchID varchar(25),TeamAbb varchar(5),PlayerName varchar(15)"
+				+ ",Pos varchar(4),PresenceTime Integer,Shootings Integer,"
+				+ "Shots Integer,ThreePointShootings Integer,ThreePointShots Integer,FreeThrowShootings Integer,"
+				+ "FreeThrowShots Integer,OffensiveRebounds Integer,DefensiveRebounds Integer,Rebounds Integer,"
+				+ "Assists Integer,Steals Integer,BlockShots Integer,TurnOvers Integer,Fouls Integer,Score Integer,primary key (MatchID,PlayerName));";
+		String TeamSql ="create table Team"+season+"Season(TeamAbb varchar(5) primary key,"
+				+ "NumOfMatch Integer,Shootings Integer,Shots Integer,ThreePointShootings Integer,"
+				+ "ThreePointShots Integer,FreeThrowShootings Integer,FreeThrowShots Integer,OffensiveRebounds Integer,"
+				+ "DefensiveRebounds Integer,Rebounds Integer,Assists Integer,Steals Integer,BlockShots Integer,TurnOvers Integer,Fouls Integer,"
+				+ "Score Integer,ShootingPersentage Double,ThreePointPersentage Double,FreeThrowPersentage Double,WinRate Double,"
+				+ "AttackRound Double,DefendRound Double,OffensiveEfficiency Double,DefensiveEfficiency Double,ReboundEfficiency Double,"
+				+ "StealEfficiency Double,AssistEfficiency Double); ";
+		if(!tableExists){
+			try {
+				stat = connection.createStatement();
+				stat.execute(MatchSql);
+				stat.execute(OvertimeSql);
+				stat.execute(PlayerSql);
+				stat.execute(PlayerMatchSql);
+				stat.execute(TeamSql);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 	}
 	
 	public static void main(String [] args){
 		DataJdbcImp data = new DataJdbcImp();
-		data.checkSeason("20142015");
+		data.checkSeason("20132014");
 	}
 	
 
